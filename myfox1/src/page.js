6c0c115d4e364ce67a7681654620294cd18deb71
anastasia <nastya16.com@gmail.com>
2023-02-02 14:31:52 +0200

    randomNumber

--- myfox1/src/page.js
+++ myfox1/src/page.js
@@ -2,7 +2,7 @@ const change_button = document.getElementById("change_button");
 const color_name = document.querySelector(".color_name");
 const colors = ['#B9F3FC', '#AEE2FF', '#93C6E7', '#FEDEFF'];
 change_button.addEventListener("click", () => {
-    console.log('click');
+    const randomColor = randomNumber();
 });
 
 

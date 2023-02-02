5befb2d5e78461cf9e80157c627660a65a983719
anastasia <nastya16.com@gmail.com>
2023-02-02 13:37:34 +0200

    first commit

--- myfox1/src/page.js
+++ myfox1/src/page.js
@@ -3,6 +3,7 @@ const color_name = document.getElementById(color_name);
 const colors = ['#B9F3FC', '#AEE2FF', '#93C6E7', '#FEDEFF'];
 change_button.addEventListener("click", () => {});
 
+
 function randomNumber(){
 
 }
